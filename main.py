6620e82e788e481485647f794d43d878086bf07e
Iga <iga.koninska@gmail.com>
2017-09-16 11:19:15 +0200

    16a commit

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ print(hello word)
 gfgfgf
 =======
 nowa zmiana
+<<<<<<< HEAD
 ej nowa
 >>>>>>> master
 kkk
+=======
+<<<<<<< Updated upstream
+ej nowa
+=======
+ej nowa
+>>>>>>> master
+kkk
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+hdhad
+>>>>>>> Stashed changes
+nowy.
+=======
+hdhad
+>>>>>>> Stashed changes
+=======
+hdhad
+>>>>>>> Stashed changes
+>>>>>>> b3e9c78... 16commit
